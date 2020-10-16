35b9af3e5b15936ecd11d68a28eef5eacd8ccad7
washixiaoqine <m16619962351@163.com>
2020-10-16 17:43:23 +0800

    新增swagger2插件

--- src/main/java/com/jl/avp/controller/UserController.java
+++ src/main/java/com/jl/avp/controller/UserController.java
@@ -4,6 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.jl.avp.model.generator.User;
 import com.jl.avp.service.UserService;
 import com.tceasy.common.base.exception.bean.ResponseBean;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +18,14 @@ import java.util.Map;
 
 @RequestMapping("user")
 @RestController
+@Api(tags = "用户管理相关接口")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("queryList")
+    @ApiOperation("查询用户列表接口")
     public Map queryList(@RequestParam(defaultValue = "1", required = false) Integer page, @RequestParam(defaultValue = "1", required = false) Integer pageSize){
         PageInfo<User> userPageInfo = userService.queryList(page, pageSize);
         return ResponseBean.SUCCESS().put("info", userPageInfo).getResult();
